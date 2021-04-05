45e7c778d06802b9f5272be0400ff4b90f51ec4a
hacan <dtmdgus901@gmail.com>
2021-04-05 19:13:50 +0900

    .

--- src/screens/ConsultRequest.js
+++ src/screens/ConsultRequest.js
@@ -18,7 +18,15 @@ export default class ConsultRequest extends Component{
         let navigation=this.props.navigation;
         return(
             <View style ={styles.container}>
-                <Text>테스트</Text>
+                 <Header style={styles.header}>
+                    <Pressable style={{position:'absolute', left:5}} onPress={()=>navigation.navigate('HomeScreen')}>
+                        <Icon style={{color:'white'}} name={'chevron-back'}/>
+                    </Pressable>
+                    <Text style={{fontSize:15, color:'white', textAlignVertical:'center'}}>상담 신청</Text>
+                </Header>
+                <View style={styles.bg}>
+                    <Text style={{marginTop:12, marginLeft: 12}}>구분</Text>
+                </View>
             </View>
         );
     }
@@ -28,5 +36,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+    },
+    header:{
+        backgroundColor:'#005A96',
+        alignItems:'center',
+    },
+    bg:{
+        width:'100%',
+        //height:'100%',
+        flexDirection:'row',
     },
 })
