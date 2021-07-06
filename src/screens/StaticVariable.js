c97c74d4c3bdfbc20b96d900eb03eaf9083105a5
turboguy <turboguy36@gmail.com>
2021-07-06 13:52:20 +0900

    api url

--- src/screens/StaticVariable.js
+++ src/screens/StaticVariable.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 'use strict';
-/*
+
 export const API_URL = 'https://www.homelandsolution.co.kr/api/api/v1/';
-*/
+/*
 export const API_URL = 'http://192.168.0.18:8085/api/v1/';
+*/
